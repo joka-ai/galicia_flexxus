1b4e3a0622651eeda2c113a559bbe3f3adf6410a
nahir rosul <nahirrosul01@gmail.com>
2026-06-02 15:57:35 -0300

    '.'

--- build.sh
+++ build.sh
@@ -3,4 +3,10 @@
 set -e
 
 pip install -r requirements.txt
-playwright install chromium --with-deps
+
+# Instalar dependencias del sistema de Chromium (build tiene acceso root)
+playwright install-deps chromium
+
+# Instalar el browser en una ruta dentro del proyecto (persiste entre builds)
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/src/.playwright-browsers
+playwright install chromium
